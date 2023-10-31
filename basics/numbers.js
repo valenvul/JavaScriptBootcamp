687ec1a7b11aab76efc77634e353477ee693bbd5
Valen Vul <valenvul@gmail.com>
2023-10-30 22:10:31 -0300

    primer dia

---
+++ basics/numbers.js
@@ -0,0 +1,8 @@
+// Challenge
+
+let studentScore = 18;
+let maxScore = 20;
+
+let percent = (studentScore/maxScore) * 100;
+
+console.log(percent);
---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+let firstName  = 'Andrew'; //declarando variables
+let lastName = 'Mead';
+let fullName = firstName + ' ' + lastName; //+ concantena strings
+
+console.log(fullName); 
+
+// challenge 1
+let city = 'Buenos Aires';
+let country = 'Argentina';
+let location = city + ', ' + country;
+
+console.log(location);
+
